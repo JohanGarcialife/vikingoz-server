d0378661661753f926d7c3a58c7f5be4fbefd307
Johan Garcia <johan.garcia165@gmail.com>
2023-09-10 16:27:55 -0400

    first commit

---
+++ src/api/aplication/controllers/aplication.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * aplication controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::aplication.aplication');
