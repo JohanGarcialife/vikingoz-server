197e89b4755609cc74baf0d51835b9cf545de222
Johan Garcia <johan.garcia165@gmail.com>
2023-09-11 13:27:50 -0400

    settings cors habilitados

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,26 +1,34 @@
 module.exports = [
-  'strapi::errors', 
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  {
+    name: "strapi::cors",
+    settings: {
+      cors: {
+        enabled: true,
+        origin: ["*"],
+      },
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 
   {
-    name: 'strapi::security',
+    name: "strapi::security",
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          "connect-src": ["'self'", "https:", ],
-          "img-src": ["'self'", "data:", "blob:","https://res.cloudinary.com" ],
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "https://res.cloudinary.com"],
           "media-src": ["'self'", "data:", "blob:"],
           upgradeInsecureRequests: null,
-           }
-           }
+        },
+      },
+    },
   },
-  }
 ];
